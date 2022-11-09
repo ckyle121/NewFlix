a0bb837814bdac3da688a7920f0aa36106ec923e
ckyle121 <carolinekyle21@gmail.com>
2022-11-09 13:39:50 -0600

    add Titanic Movie class and make it the best Movie

--- src/main/java/com/javaunit3/springmvc/BestMovieService.java
+++ src/main/java/com/javaunit3/springmvc/BestMovieService.java
@@ -1,13 +1,14 @@
 package com.javaunit3.springmvc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BestMovieService {
     private Movie movie;
     @Autowired
-    public BestMovieService(Movie movie){
+    public BestMovieService(@Qualifier("titanicMovie") Movie movie){
         this.movie = movie;
     }
 
